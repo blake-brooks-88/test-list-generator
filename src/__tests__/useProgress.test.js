715f09817718e3599ccfd139505516d7e068ec20
bbrookslistengage <bbrooks@listengage.com>
2025-06-12 18:03:20 -0500

    Implement progress provider hook with tests

---
+++ src/__tests__/useProgress.test.js
@@ -0,0 +1,162 @@
+import { renderHook, act, render } from '@testing-library/react'
+import { useProgress, ProgressProvider } from '../hooks/useProgress'
+
+const renderUseProgress = () => {
+    const wrapper = ({ children }) => (
+        <ProgressProvider>
+            {children}
+        </ProgressProvider>
+    )
+    return renderHook(() => useProgress(), { wrapper });
+}
+
+describe('useProgress', () => {
+    test('currentStep initial state is 1', () => {
+        const { result } = renderUseProgress();
+
+        expect(result.current.currentStep).toBe(1);
+    })
+
+    test('nextStep increments currentStep from 1 to 2', () => {
+        const { result } = renderUseProgress();
+
+        expect(result.current.currentStep).toBe(1);
+
+        act(() => {
+            result.current.nextStep();
+        });
+
+        expect(result.current.currentStep).toBe(2);
+    })
+
+    test('nextStep doesnt increment when current step is equal to 4', () => {
+        const { result } = renderUseProgress();
+
+        expect(result.current.currentStep).toBe(1);
+
+        act(() => {
+            result.current.nextStep();
+            result.current.nextStep();
+            result.current.nextStep();
+        });
+
+        expect(result.current.currentStep).toBe(4);
+
+        act(() => {
+            result.current.nextStep();
+        });
+
+        expect(result.current.currentStep).toBe(4);
+    })
+
+    test('previousStep decrements currentStep from 4 to 3', () => {
+        const { result } = renderUseProgress();
+
+        expect(result.current.currentStep).toBe(1);
+
+        act(() => {
+            result.current.nextStep();
+            result.current.nextStep();
+            result.current.nextStep();
+        });
+
+        expect(result.current.currentStep).toBe(4);
+
+        act(() => {
+            result.current.previousStep();
+        });
+
+        expect(result.current.currentStep).toBe(3);
+    })
+
+    test('previousStep doesnt decrement currentStep when current step is = 1', () => {
+        const { result } = renderUseProgress();
+
+        expect(result.current.currentStep).toBe(1);
+
+        act(() => {
+            result.current.previousStep();
+        });
+
+        expect(result.current.currentStep).toBe(1);
+
+    })
+
+    test('goToStep goes to specified step', () => {
+        const { result } = renderUseProgress();
+
+        expect(result.current.currentStep).toBe(1);
+
+        act(() => {
+            result.current.nextStep();
+            result.current.nextStep();
+            result.current.nextStep();
+        });
+
+        expect(result.current.currentStep).toBe(4);
+
+        act(() => {
+            result.current.goToStep(2);
+        });
+
+        expect(result.current.currentStep).toBe(2);
+
+    })
+
+    test('goToStep cannot go forward a step', () => {
+        const { result } = renderUseProgress();
+
+        expect(result.current.currentStep).toBe(1);
+
+        act(() => {
+            result.current.goToStep(2);
+        });
+
+        expect(result.current.currentStep).toBe(1);
+
+    })
+
+    test('goToStep cannot go below 0', () => {
+        const { result } = renderUseProgress();
+
+        expect(result.current.currentStep).toBe(1);
+
+        act(() => {
+            result.current.goToStep(-1);
+        });
+
+        expect(result.current.currentStep).toBe(1);
+
+        act(() => {
+            result.current.goToStep(0);
+        });
+
+        expect(result.current.currentStep).toBe(1);
+
+        act(() => {
+            result.current.goToStep(-999);
+        });
+
+        expect(result.current.currentStep).toBe(1);
+
+    })
+
+    test('goToStep only accepts integers', () => {
+        const { result } = renderUseProgress();
+
+        expect(result.current.currentStep).toBe(1);
+
+        act(() => {
+            result.current.goToStep(2.5);
+        });
+
+        expect(result.current.currentStep).toBe(1);
+
+        act(() => {
+            result.current.goToStep("testing testing");
+        });
+
+        expect(result.current.currentStep).toBe(1);
+
+    })
+})
---
+++ src/hooks/useProgress.js
@@ -0,0 +1,53 @@
+import React, { createContext, useState, useContext } from 'react'
+
+const UseProgressContext = createContext();
+
+export const ProgressProvider = ({ children }) => {
+    const [currentStep, setCurrentStep] = useState(1);
+    const MAX_STEPS = 4;
+
+    const nextStep = () => {
+        if (currentStep < MAX_STEPS) {
+            setCurrentStep(prev => prev + 1);
+        }
+    }
+
+    const previousStep = () => {
+        if (currentStep > 1) {
+            setCurrentStep(prev => prev - 1);
+        }
+    }
+
+    const goToStep = (stepNumber) => {
+        if (canGoToStep(stepNumber)) {
+            setCurrentStep(stepNumber)
+        }
+    }
+
+    const canGoToStep = (stepNumber) => {
+        if (stepNumber > currentStep) return false;
+        if (stepNumber < 1) return false;
+        if (stepNumber % 1 !== 0) return false;
+
+        return true;
+    }
+    return (
+        <UseProgressContext.Provider
+            value={{
+                currentStep,
+                nextStep,
+                previousStep,
+                goToStep,
+            }}>
+            {children}
+        </UseProgressContext.Provider>
+    )
+}
+
+export const useProgress = () => {
+    const context = useContext(UseProgressContext);
+    if (!context) {
+        throw new Error('useProgress must be used within ProgressProvider')
+    }
+    return context;
+}
